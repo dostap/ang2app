fa4b2b961a76a8f20f353620753d353fac98d927
dostap <dig14step@gmail.com>
2016-11-24 11:11:54 -0700

    section 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StocksComponent } from './stocks.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MutualfundsComponent } from './mutualfunds/mutualfunds.component';
+import { StocksComponent } from './stocks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MutualfundsComponent,
+    StocksComponent
   ],
   imports: [
     BrowserModule,
